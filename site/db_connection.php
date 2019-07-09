84b03e625bcc2ce2bd8231db6c39eb694b9f95af
Patrick Szalewicz <git@fachinformatiker.app>
2019-07-09 08:52:36 -0400

    added test.html for styling purposes

--- site/db_connection.php
+++ site/db_connection.php
@@ -1,7 +1,7 @@
 <?php
 $dbhost = "localhost";
-$dbuser = "pszalewicz";
-$dbpass = "01020102..aA";
+$dbuser = "USER";
+$dbpass = "PASS";
 $db = "foodtracker";
 $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
 ?>
